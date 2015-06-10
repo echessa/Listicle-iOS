f474f27927d04bacb270682c6fab0bcc2233ef0f
Joyce Echessa <jokhessa@yahoo.com>
2015-06-10 07:44:45 +0300

    Initial Commit

---
+++ Listicle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Listicle
+//
+//  Created by Joyce Echessa on 6/10/15.
+//  Copyright (c) 2015 Echessa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
